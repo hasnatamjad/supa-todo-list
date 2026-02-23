1507860ffa7856ea8c8b681e38fd962e5ec7dc56
Muhammad Hasnat <98840144+hasnatamjad@users.noreply.github.com>
2026-02-23 20:12:49 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { DashboardLayout } from "@/components/DashboardLayout";
 import Auth from "./pages/Auth";
 import TaskRegistry from "./pages/TaskRegistry";
 import TaskList from "./pages/TaskList";
+import Members from "./pages/Members";
+import Inbox from "./pages/Inbox";
 import FinishedTasks from "./pages/FinishedTasks";
 import ExpenseTracker from "./pages/ExpenseTracker";
 import NotFound from "./pages/NotFound";
@@ -27,6 +29,9 @@ const App = () => (
             <Route path="/task-list" element={<DashboardLayout><TaskList /></DashboardLayout>} />
             <Route path="/finished" element={<DashboardLayout><FinishedTasks /></DashboardLayout>} />
             <Route path="/expenses" element={<DashboardLayout><ExpenseTracker /></DashboardLayout>} />
+      <Route path="members" element={<Members />} />
+      <Route path="inbox" element={<Inbox />} />
+
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
